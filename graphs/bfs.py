4d83d78a73acac68203f77a4b523942246487e5d
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-09-13 00:44:13 +1000

    modified breadth first search

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -20,15 +20,18 @@ def bfs(graph, start_node):
     print(current_node)
     visited_node_set.add(current_node)
     for adjacent_node in graph[current_node]:
-        node_queue.append(adjacent_node)
+        if adjacent_node not in visited_node_set:
+            node_queue.append(adjacent_node)
+            print(adjacent_node)
+            visited_node_set.add(adjacent_node)
 
-    while len(node_queue) != 0:
+    while len(node_queue):
         current_node = node_queue.popleft()
-        if current_node not in visited_node_set:
-            print(current_node)
-            visited_node_set.add(current_node)
-            for adjacent_node in graph[current_node]:
+        for adjacent_node in graph[current_node]:
+            if adjacent_node not in visited_node_set:
                 node_queue.append(adjacent_node)
+                print(adjacent_node)
+                visited_node_set.add(adjacent_node)
 
 
 if __name__ == "__main__":
